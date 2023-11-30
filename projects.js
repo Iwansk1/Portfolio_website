3c6e3b8bf4d5ffab545a440d6401baf96f2464d6
Iwansk1 <Iwanbijl@gmail.com>
2023-11-30 10:56:32 +0100

    .

---
+++ projects.js
@@ -0,0 +1,14 @@
+// gsap.registerEffect({
+//     name: "fadein",
+//     defaults: {duratiom: 2},
+//     effect: (targets, config) => {
+//         return gsap.to(targets, { duration: config.duration, opacity: 0});
+//     }
+// });
+// gsap.effects.fadein(".projects_text");
+
+// document.querySelectorAll(".projects_text").forEach(function(fade) {
+//     fade.addEventListener("mouseenter", function() {
+//       gsap.effects.fadein(this);
+//     });
+//   });
