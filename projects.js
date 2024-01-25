c43b7c12a6cd33c1095f7933bcf91d6f21028b17
Iwansk1 <Iwanbijl@gmail.com>
2024-01-25 13:23:01 +0100

    .

--- projects.js
+++ projects.js
@@ -12,18 +12,22 @@ toggleBtn.onclick = function(){
 }
 
 
-
+function showAllImages(contaniner){
+    const images = document.querySelectorAll('.projects_box')
+    images.forEach(image =>{
+        image.style.display = 'flex'
+        image.style.opacity = '1'
+    })
+}
 function filterImages(category){
     const imageContainers = document.querySelectorAll('.projects_box');
     
     imageContainers.forEach(container => {
         if (container.classList.contains(category)){
             container.style.display = 'flex'
-            container.style.opacity = '1'
             enableImages(container)
         } else{
             container.style.display = 'none'
-            container.style.opacity = '0'
             disableImages(container)
         }
     });
@@ -33,6 +37,10 @@ function enableImages(container){
     const images = container.querySelectorAll('.projects_img')
     images.forEach(image =>{
         image.removeAttribute('disabled')
+        container.style.opacity = '1'
+        setTimeout(() => {
+            container.style.opacity = '1';
+        }, 50);
     })
 }
 
@@ -40,5 +48,9 @@ function disableImages(container){
     const images = container.querySelectorAll('.projects_img')
     images.forEach(image =>{
         image.setAttribute('disabled', true)
+        container.style.opacity = '0'
+        setTimeout(() => {
+            container.style.opacity = '0';
+        }, 150);
     })
 }
